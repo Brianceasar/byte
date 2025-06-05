0ee4e4f4f8ec05e14d2407811a78787812b2fc61
brianceasar <brianceasar7@gmail.com>
2025-06-05 22:52:16 +0300

    add auth controller

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.*;
+import com.example.demo.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        String message = authService.register(request);
+        return ResponseEntity.ok(message);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+}
