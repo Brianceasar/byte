7510c234f0e22129b1c157afab1746d94aefa44b
brianceasar <brianceasar7@gmail.com>
2025-06-05 21:31:52 +0300

    add user entity

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
