ac809cb031a67d20264cef3422edacd7a7c9eda3
brianceasar <brianceasar7@gmail.com>
2025-06-08 16:08:36 +0300

    add unauth page

---
+++ client/app/unauthorized/page.tsx
@@ -0,0 +1,8 @@
+const Unauthorized = () => (
+  <div className="p-6 text-center">
+    <h1 className="text-3xl font-bold text-red-600">403 - Unauthorized</h1>
+    <p className="mt-4">You do not have access to this page.</p>
+  </div>
+)
+
+export default Unauthorized
