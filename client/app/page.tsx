51e505785cd6d1d00f66f0649a1aa8b380fcbd96
brianceasar <brianceasar7@gmail.com>
2025-06-13 16:05:45 +0300

    optimised the admin page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -22,12 +22,7 @@ const Home = () => {
             </Link>
           )}
 
-          <button
-            onClick={logout}
-            className="mt-4 bg-red-600 text-white px-4 py-2 rounded"
-          >
-            Logout
-          </button>
+          
         </>
       ) : (
         <>
