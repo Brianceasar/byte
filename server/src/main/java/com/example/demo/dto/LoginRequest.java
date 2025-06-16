1fcfdb6f3dbeb260163c02c7cd330beb11a37b2e
brianceasar <brianceasar7@gmail.com>
2025-06-16 22:17:11 +0300

    refactor(dto): remove redundant comments from LoginRequest for clarity

--- server/src/main/java/com/example/demo/dto/LoginRequest.java
+++ server/src/main/java/com/example/demo/dto/LoginRequest.java
@@ -4,16 +4,13 @@ public class LoginRequest {
     private String email;
     private String password;
 
-    // Default constructor
     public LoginRequest() {}
 
-    // Constructor with all fields
     public LoginRequest(String email, String password) {
         this.email = email;
         this.password = password;
     }
 
-    // Getters and Setters
     public String getEmail() {
         return email;
     }
